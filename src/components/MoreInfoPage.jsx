525fd681c7788536289902ca5d8b744c7a60c0a2
Alex Almanza <44lmanza@gmail.com>
2022-06-24 11:53:07 -0700

    Create MoreInfoPage component

---
+++ src/components/MoreInfoPage.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+function MoreInfoPage() {
+  return (
+    <div className='home-more-stuff'>
+      <div className='more-stuff-grid'>
+        <img
+          src='https://unsplash.it/400'
+          alt=''
+          className='slide-in from-left'
+        />
+        <p className='slide-in from-right'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
+          quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
+          omnis sapiente neque cumque consequuntur reiciendis deserunt. Dolorem
+          vero exercitationem consequuntur, eligendi cupiditate debitis facilis
+          quibusdam magni. Eveniet.
+        </p>
+      </div>
+      <div className='more-stuff-grid'>
+        <p className='slide-in from-left'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
+          quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
+          omnis sapiente neque cumque consequuntur reiciendis deserunt. Dolorem
+          vero exercitationem consequuntur, eligendi cupiditate debitis facilis
+          quibusdam magni. Eveniet.
+        </p>
+        <img
+          src='https://unsplash.it/401'
+          alt=''
+          className='slide-in from-right'
+        />
+      </div>
+      <div className='more-stuff-grid'>
+        <img
+          src='https://unsplash.it/400'
+          alt=''
+          className='slide-in from-left'
+        />
+        <p className='slide-in from-right'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
+          quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
+          omnis sapiente neque cumque consequuntur reiciendis deserunt. Dolorem
+          vero exercitationem consequuntur, eligendi cupiditate debitis facilis
+          quibusdam magni. Eveniet.
+        </p>
+      </div>
+      <div className='more-stuff-grid'>
+        <p className='slide-in from-left'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
+          quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
+          omnis sapiente neque cumque consequuntur reiciendis deserunt. Dolorem
+          vero exercitationem consequuntur, eligendi cupiditate debitis facilis
+          quibusdam magni. Eveniet.
+        </p>
+        <img
+          src='https://unsplash.it/401'
+          alt=''
+          className='slide-in from-right'
+        />
+      </div>
+      <div className='more-stuff-grid'>
+        <img
+          src='https://unsplash.it/400'
+          alt=''
+          className='slide-in from-left'
+        />
+        <p className='slide-in from-right'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
+          quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
+          omnis sapiente neque cumque consequuntur reiciendis deserunt. Dolorem
+          vero exercitationem consequuntur, eligendi cupiditate debitis facilis
+          quibusdam magni. Eveniet.
+        </p>
+      </div>
+      <div className='more-stuff-grid'>
+        <p className='slide-in from-left'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
+          quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
+          omnis sapiente neque cumque consequuntur reiciendis deserunt. Dolorem
+          vero exercitationem consequuntur, eligendi cupiditate debitis facilis
+          quibusdam magni. Eveniet.
+        </p>
+        <img
+          src='https://unsplash.it/401'
+          alt=''
+          className='slide-in from-right'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MoreInfoPage;
