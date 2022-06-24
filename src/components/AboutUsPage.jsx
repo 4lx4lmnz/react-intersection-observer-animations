01662cc6443c9633e6b9720cda07d00f08c2f8e5
Alex Almanza <44lmanza@gmail.com>
2022-06-24 11:50:28 -0700

    Create AboutUsPage component

---
+++ src/components/AboutUsPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function AboutUsPage() {
+  return (
+    <div className='home-about'>
+      <h2>About us</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      <div className='columns'>
+        <div className='col fade-in'>
+          <h3>Lorem, ipsum.</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+            maiores fuga eos provident voluptas perferendis.
+          </p>
+        </div>
+        <div className='col fade-in'>
+          <h3>A, illo!</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus minima quo beatae eius blanditiis officiis.
+          </p>
+        </div>
+        <div className='col fade-in'>
+          <h3>Repudiandae, error?</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            quasi quis doloribus quia illum laudantium.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUsPage;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutUsPage from './AboutUsPage';
 import SplashPage from './SplashPage';
 
 function Main() {
@@ -6,33 +7,7 @@ function Main() {
     <main>
       <SplashPage />
 
-      <div className='home-about'>
-        <h2>About us</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-        <div className='columns'>
-          <div className='col fade-in'>
-            <h3>Lorem, ipsum.</h3>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
-              maiores fuga eos provident voluptas perferendis.
-            </p>
-          </div>
-          <div className='col fade-in'>
-            <h3>A, illo!</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus minima quo beatae eius blanditiis officiis.
-            </p>
-          </div>
-          <div className='col fade-in'>
-            <h3>Repudiandae, error?</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
-              quasi quis doloribus quia illum laudantium.
-            </p>
-          </div>
-        </div>
-      </div>
+      <AboutUsPage />
 
       <div className='home-more-stuff'>
         <div className='more-stuff-grid'>
