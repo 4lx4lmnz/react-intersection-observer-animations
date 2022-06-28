36e76a38e61296e62b058afc1cc5da0f81519ee3
Alex Almanza <44lmanza@gmail.com>
2022-06-28 09:45:40 -0700

    Implement inView for about us column

--- src/components/AboutUsPage.jsx
+++ src/components/AboutUsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { InView } from 'react-intersection-observer';
 
 function AboutUsPage() {
   return (
@@ -6,13 +7,17 @@ function AboutUsPage() {
       <h2>About us</h2>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
       <div className='columns'>
-        <div className='col fade-in'>
-          <h3>Lorem, ipsum.</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
-            maiores fuga eos provident voluptas perferendis.
-          </p>
-        </div>
+        <InView threshold={0} rootMargin='0px 0px -250px 0px'>
+          {({ inView, ref }) => (
+            <div ref={ref} className={`col fade-in ${inView && 'appear'}`}>
+              <h3>Lorem, ipsum.</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+                maiores fuga eos provident voluptas perferendis.
+              </p>
+            </div>
+          )}
+        </InView>
         <div className='col fade-in'>
           <h3>A, illo!</h3>
           <p>
