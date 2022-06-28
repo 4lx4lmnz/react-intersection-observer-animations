5d02da83b551112b880b6ddda7aea8b84e3b04c2
Alex Almanza <44lmanza@gmail.com>
2022-06-28 09:19:42 -0700

    Refactor useInView usage

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -2,11 +2,10 @@ import React, { useCallback, useEffect, useRef } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 function SplashPage({ onChange }) {
-  const intersectionOpts = {
+  const { ref: sectionRef } = useInView({
     rootMargin: '-200px 0px 0px 0px',
     onChange: onChange,
-  };
-  const { ref: sectionRef, inView } = useInView(intersectionOpts);
+  });
 
   return (
     <section className='home-intro' ref={sectionRef}>
