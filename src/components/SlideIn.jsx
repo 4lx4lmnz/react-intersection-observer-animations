82092d03d554eddf7e0a32a25f2dc0a037854614
Alex Almanza <44lmanza@gmail.com>
2022-06-29 07:51:50 -0700

    Add SlideIn component

---
+++ src/components/SlideIn.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { InView } from 'react-intersection-observer';
+
+function SlideIn({ children, fromRight = true }) {
+  const handleInView = (inView, entry) => {
+    if (inView) {
+      entry.target.classList.add('appear');
+    }
+  };
+
+  console.log(children);
+
+  return (
+    <InView
+      as='div'
+      className={fromRight ? 'from-right' : 'from-left'}
+      onChange={handleInView}
+      threshold={0}
+      rootMargin='0px 0px -250px 0px'
+    >
+      {children}
+    </InView>
+  );
+}
+
+export default SlideIn;
