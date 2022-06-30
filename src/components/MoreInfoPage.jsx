5128862ba03eebe2a47f46d419474ae0ce12d6e7
Alex Almanza <44lmanza@gmail.com>
2022-06-30 09:49:06 -0700

    Refactor MoreInfoPage.jsx

--- src/components/MoreInfoPage.jsx
+++ src/components/MoreInfoPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import DualSlideIn from './animations/DualSlideIn';
-import SlideIn from './animations/SlideIn';
 
 function MoreInfoPage() {
   return (
@@ -28,18 +27,6 @@ function MoreInfoPage() {
           </p>
         }
         right={<img src='https://unsplash.it/401' alt='' />}
-      />
-      <DualSlideIn
-        left={<img src='https://unsplash.it/400' alt='' />}
-        right={
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis fugit,
-            quae beatae vero sit magni quaerat id ratione. Dolor optio unde amet
-            omnis sapiente neque cumque consequuntur reiciendis deserunt.
-            Dolorem vero exercitationem consequuntur, eligendi cupiditate
-            debitis facilis quibusdam magni. Eveniet.
-          </p>
-        }
       />
       <DualSlideIn
         left={<img src='https://unsplash.it/400' alt='' />}
