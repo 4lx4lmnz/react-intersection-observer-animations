5fed427bc997aa856f6cb7166acacf77d6bebe27
Alex Almanza <44lmanza@gmail.com>
2022-06-30 11:16:51 -0700

    Use inline styles

--- src/components/animations/SlideIn.jsx
+++ src/components/animations/SlideIn.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InView } from 'react-intersection-observer';
 import PropTypes from 'prop-types';
 
+const styles = {
+  rightStart: {
+    gridColumn: '3 / 4',
+    transform: 'translateX(50%)',
+    opacity: 0,
+  },
+  leftStart: {
+    gridColumn: '2 / 3',
+    transform: 'translateX(-50%)',
+    opacity: 0,
+  },
+  appear: {
+    transform: 'translateX(0)',
+    opacity: 1,
+  },
+  transitions: {
+    transition: 'opacity 250ms ease-in, transform 400ms ease-in',
+  },
+};
+
 function SlideIn({ children, fromRight = true }) {
+  const [hasAppeared, setHasAppeared] = useState(false);
+
   const handleInView = (inView, entry) => {
     if (inView) {
-      entry.target.classList.add('appear');
+      setHasAppeared(true);
     }
+  };
+
+  const getStyle = () => {
+    return Object.assign(
+      {},
+      fromRight ? styles.rightStart : styles.leftStart,
+      styles.transitions,
+      hasAppeared && styles.appear
+    );
   };
 
   return (
     <InView
       as='div'
-      className={`slide-in ${fromRight ? 'from-right' : 'from-left'}`}
+      style={getStyle()}
       onChange={handleInView}
       threshold={0}
       rootMargin='0px 0px -250px 0px'
