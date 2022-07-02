c6bc0e848b5f2534b69d2c216a7dc2ed12b2b05c
Alex Almanza <44lmanza@gmail.com>
2022-07-02 10:37:24 -0700

    Setup styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
-import './App.scss';
+import { ThemeProvider } from 'styled-components';
 import HomePage from './components/HomePage';
 import Navbar from './components/Navbar';
+import GlobalStyles from './components/styles/Global';
+
+const MY_THEME = {
+  text: { default: '#f4f4f4', inverse: '#333' },
+  background: 'transparent',
+  spacing: '1em',
+};
 
 function App() {
   const [navScrolled, setNavScrolled] = useState(false);
 
   return (
-    <>
-      <Navbar scrolled={navScrolled} />
+    <ThemeProvider theme={MY_THEME}>
+      <>
+        <GlobalStyles />
 
-      <HomePage
-        onChange={(inView) => {
-          if (inView) {
-            setNavScrolled(false);
-          } else {
-            setNavScrolled(true);
-          }
-        }}
-      />
-    </>
+        <Navbar scrolled={navScrolled} />
+
+        <HomePage
+          onChange={(inView) => {
+            if (inView) {
+              setNavScrolled(false);
+            } else {
+              setNavScrolled(true);
+            }
+          }}
+        />
+      </>
+    </ThemeProvider>
   );
 }
 
